66335e0276d714788553b173b437bb3893b7de2e
KimHyungSik <goust2001c@naver.com>
2021-06-10 10:24:23 +0900

    add preferencesManager

---
+++ app/src/main/java/com/example/plasticx/utils/PreferencesManager.kt
@@ -0,0 +1,35 @@
+package com.example.plasticx.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferencesManager {
+    public val PREFERENCES_NAME = "PlasticX"
+    val DEFAULT_VALUE_STRING = ""
+
+    private fun getPreferences (context : Context) : SharedPreferences {
+        return context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
+    }
+
+    //String 데이터 저장
+    fun setString(context : Context, key : String, value : String){
+        val prefs = getPreferences(context)
+        val editor = prefs.edit()
+        editor.putString(key, value)
+        editor.apply()
+    }
+
+    //String 데이터 가져오기
+    fun getString (context : Context, key : String) : String{
+        val prefs = getPreferences(context)
+        val value = prefs.getString(key, DEFAULT_VALUE_STRING)
+        return value ?: ""
+    }
+
+    fun removeKey(context : Context, key : String){
+        val prefs = getPreferences(context)
+        val editor = prefs.edit()
+        editor.remove(key)
+        editor.apply()
+    }
+}
