fe334aeb71687ca15c5983fa5abea1f51bfd04b2
KimHyungSik <goust2001c@naver.com>
2021-07-03 22:18:05 +0900

    update borrowTumbler api

--- app/src/main/java/com/example/plasticx/qr/QrViewModel.kt
+++ app/src/main/java/com/example/plasticx/qr/QrViewModel.kt
@@ -8,23 +8,37 @@ import com.example.plasticx.retrofit.repository.RetrofitRepository
 import com.google.gson.JsonElement
 import javax.inject.Inject
 
-
 class QrViewModel @Inject constructor(val retrofitRepository: RetrofitRepository) {
 
     val TAG = "QrViewModel - 로그"
 
     fun borrowTumbler(tumblerId: String) {
         retrofitRepository.borrowTumblerRx(tumblerId)
-            .map{it -> {
-                Log.d(TAG, "borrowTumbler: ${it.asJsonObject.asString}")
-            }}
+            .map { it.asJsonObject }
             .subscribe(
-                { Log.d(TAG, "borrowTumbler: $it") },
                 {
-                    Log.d(TAG, "borrowTumbler: Error $it")
+                    when (it.get("RESULT").asString) {
+                        "200" -> {
+                            Log.d(TAG, "borrowTumbler: 텀블러 대여 성공")
+                        }
+                        "300" -> {
+                            Log.d(TAG, "borrowTumbler: 보증금 부족")
+                        }
+                        "301" -> {
+                            Log.d(TAG, "borrowTumbler: 텁블러 대여 중")
+                        }
+                        else -> {
+                            Log.d(TAG, "borrowTumbler: Error")
+                        }
+                    }
                 },
-                { Log.d(TAG, "borrowTumbler: Complete") }
+                {
+                    Log.d(TAG, "borrowTumbler: Error ${it}")
+                },
+                {
+
+                    Log.d(TAG, "borrowTumbler: Complete")
+                }
             ).isDisposed
     }
-
 }
